13ec7230b7012380c84b1702e0bb46b8b81e17bc
SandyTwix <iamsandytwix@gmail.com>
2022-12-08 15:46:54 +0300

    Initial Commit

---
+++ EffectiveMobileTest/EffectiveMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestApp.swift
+//  EffectiveMobileTest
+//
+//  Created by Руслан Трищенков on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
